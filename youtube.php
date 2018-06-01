ec2fd6128a3e23843aa9f3d84a7a3002947c8b71
Awilum <awilum@msn.com>
2018-06-01 20:59:46 +0300

    typo fixes

--- youtube.php
+++ youtube.php
@@ -30,5 +30,5 @@ Event::addListener('onShortcodesInitialized', function () {
 
 function youtube(string $url, int $width = 560, int $height = 315) : string
 {
-    return '<iframe width="'.$width.'" height="'.$height.'" src="https://www.youtube.com/embed/'.substr($source, strpos($source, "=") + 1).'" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+    return '<iframe width="'.$width.'" height="'.$height.'" src="https://www.youtube.com/embed/'.substr($url, strpos($url, "=") + 1).'" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
 }
